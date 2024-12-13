d5e76bbd43be6d365df44b2a3942cf862a2176c1
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2024-12-13 13:29:15 -0300

    Adiciona código base ao vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ufjf-dcc207-2024-3a-atv05-luizacaldeira",
+  base: "/ufjf-dcc207-2024-3-a-atv05-luizacaldeira",
   plugins: [react()],
 })
